e390d6ce9a3247e6bfa9ad4fd918f216f7ca27a7
Remzeey <remzwisdom@gmail.com>
2022-11-17 13:13:46 +0100

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className=" h-fit p-5 w-screen  bg-slate-800  text-white ">
+      <div className="m-1">
+        
+            <p className="cursor-pointer text-gray-500 w-fit hover:text-white">
+              About us
+            </p>
+         
+            <p className="cursor-pointer text-gray-500 w-fit hover:text-white">
+              Contact us
+            </p>
+        
+      </div>
+      <p className="flex justify-end  m-1  sm:text-xl font-bold ">
+        Connect with us on
+      </p>
+      <div className=" flex justify-end mb-12 ">
+        <SocialIcon
+          url="https://facebook.com/MaximumRubix"
+          style={{ height: 25, width: 25, margin: 1 }}
+        />
+        <SocialIcon
+          url="https://instagram.com/Maximum_Rubix"
+          style={{ height: 25, width: 25, margin: 1 }}
+        />
+        <SocialIcon
+          url="https://whatsapp.com"
+          style={{ height: 25, width: 25, margin: 1 }}
+        />
+      </div>
+
+      <div className=" text-center  ">Created By &copy; 2022 Remzeey</div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <div className=" bg-slate-900 text-black w-full h-14">
+          <div className='p-2 font-bold bg-white max-w-fit '>
+              MAXIMUM RUBIX
+          </div>
+    </div>
+  )
+}
+
+export default Header
